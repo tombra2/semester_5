c882a9afd3b1adf40f88599be7d58a22fc16b24d
Brandner <brandner@akaryon.com>
2025-09-29 18:11:53 +0200

    Main OOP

--- OOP_Calculations/src/Main.java
+++ OOP_Calculations/src/Main.java
@@ -1,12 +1,10 @@
 public class Main {
     static void main() {
 
-
         Number num1 = new Number(4);
         Number num2 = new Number(3);
         Calculation c1 = new Calculation(num1, num2, new Division());
         System.out.println(c1);
-
 
     }
 }
